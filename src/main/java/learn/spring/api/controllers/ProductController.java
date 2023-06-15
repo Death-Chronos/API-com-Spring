995eca7ea6661d6d8e9395f9b501e40505c6518d
João Victor Costa <80175567+Death-Chronos@users.noreply.github.com>
2023-06-15 16:15:59 -0300

    Procurar só um Produto

--- src/main/java/learn/spring/api/controllers/ProductController.java
+++ src/main/java/learn/spring/api/controllers/ProductController.java
@@ -1,9 +1,15 @@
 package learn.spring.api.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +30,21 @@ public class ProductController {
         var productModel = new ProductModel(); // Cria um objeto produto, var é uma nova função do Java 10, que permite que a definição de tipo seja feita apenas no lado depois do =, quase como o JS
         BeanUtils.copyProperties(productRecordDTO, productModel); //Faz a transformação ProductDTO para ProductModel
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel)); //Utilizando o protocolo Http. Basicamente retorna um codigo http falando que foi enviado, e manda o objeto que foi salvo também
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getALlProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll()); //retorna o estado HTTP, junto com todos os produtos cadastrados
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value="id") UUID id){
+        Optional<ProductModel> product0  = productRepository.findById(id); //Procura o produto no repositorio usando o ID
+
+        if(product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found"); //verifica se recebeu alguma coisa, caso não, retorna que não achou.
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get()); //Retorna o produto caso o tenha achado.
     }
 
 
