cdf29869552ee59a651610d1dee80ea3894aaa2b
JV <joaovictorcunha12102002@gmail.com>
2024-08-04 14:32:14 -0300

    RoleRepository

--- src/main/java/learn/spring/api/repositorys/RoleRepository.java
+++ src/main/java/learn/spring/api/repositorys/RoleRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import learn.spring.api.models.Role;
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
-    
+    boolean existsByName(String name);
+
+    Role findByName(String string);
 }
